164267db5fd6bcea84169e0bf42cf3dd90a8fc01
Jabo Fecht <jifecht@gmail.com>
2023-05-12 13:56:45 +0200

    Refactor projects jsx and add contact me button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,32 @@ import BurgerIcon from "./components/icons/BurgerIcon"
 import GithubIcon from "./components/icons/GithubIcon"
 
 export default function Home() {
+	const projects = [
+		{
+			name: "Pomodoro app",
+			projectURL: "https://pomodoro.jabofecht.com",
+			githubURL: "https://github.com/JobaF/pomodoro-app",
+			tailwindImageTag: "bg-pomodoro",
+		},
+		{
+			name: "Dictionary app",
+			projectURL: "https://dictionary.jabofecht.com/dictionary/keyboard",
+			githubURL: "https://github.com/JobaF/dictionary-app",
+			tailwindImageTag: "bg-dictionary",
+		},
+		{
+			name: "Age calculator",
+			projectURL: "https://datecalculator.jabofecht.com/",
+			githubURL: "https://github.com/JobaF/frontendmentor-age-calculator",
+			tailwindImageTag: "bg-clock",
+		},
+		{
+			name: "Apple-style calculator",
+			projectURL: "https://jobaf.github.io/odin-calculator/",
+			githubURL: "https://github.com/JobaF/odin-calculator",
+			tailwindImageTag: "bg-calculator",
+		},
+	]
 	return (
 		<div className="flex flex-col bg-gray-900 font-crimson text-white">
 			<nav className="z-10 flex h-14 items-center justify-between bg-gray-800 px-8">
@@ -33,62 +59,33 @@ export default function Home() {
 					</h3>
 					<p className="mt-2 text-gray-400">Frontend Developer</p>
 					<p className="text-gray-400">M. Sc. Information Systems</p>
-					<button className="mt-4 h-12 w-32 rounded-xl bg-gradient-to-r from-sky-500 to-indigo-500 drop-shadow-xl hover:bg-gradient-to-r hover:from-sky-800 hover:to-indigo-800">
-						Download CV
-					</button>
+					<a
+						href="mailto:hi@jabofecht.com"
+						className="mt-4 flex items-center justify-center"
+					>
+						<div className="flex h-12 w-32 items-center justify-center rounded-xl bg-gradient-to-r from-sky-500 to-indigo-500 drop-shadow-xl hover:bg-gradient-to-r hover:from-sky-800 hover:to-indigo-800 hover:shadow-inner hover:shadow-black">
+							Contact me
+						</div>
+					</a>
 				</div>
 			</section>
 
 			<section className="mb-12 mt-20 flex flex-col items-center">
 				<div className="text-3xl underline">My Projects</div>
-				<div className="relative mt-8">
-					<div className=" h-72 w-72 items-start justify-center overflow-hidden rounded-lg">
-						<div className="absolute inset-0  rounded-lg bg-orange-600 opacity-70 blur-md"></div>
-						<div className="relative inset-0 flex h-full w-full items-start justify-between rounded-md bg-pomodoro bg-cover px-3 pt-2 text-left">
-							<span className="text-xl ">Pomodoro app</span>
-							<ArrowTopRightIcon url={"https://pomodoro.jabofecht.com"} />
-							<GithubIcon url={"https://github.com/JobaF/pomodoro-app"} />
+				{projects.map((project) => (
+					<div className="relative mt-8">
+						<div className=" h-72 w-72 items-start justify-center overflow-hidden rounded-lg">
+							<div className="absolute inset-0  rounded-lg bg-black opacity-70 blur-md"></div>
+							<div
+								className={`${project.tailwindImageTag} relative inset-0 flex h-full w-full items-start justify-between rounded-md bg-cover px-3 pt-2 text-left`}
+							>
+								<span className="text-xl ">{project.name}</span>
+								<ArrowTopRightIcon url={project.projectURL} />
+								<GithubIcon url={project.githubURL} />
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className="relative mt-8">
-					<div className=" h-72 w-72 items-start justify-center overflow-hidden rounded-lg">
-						<div className="absolute inset-0  rounded-lg bg-black opacity-70 blur-md"></div>
-						<div className="relative inset-0 flex h-full w-full items-start justify-between rounded-md bg-dictionary bg-cover px-3 pt-2 text-left">
-							<span className="text-xl ">Dictionary app</span>
-							<ArrowTopRightIcon
-								url={"https://dictionary.jabofecht.com/dictionary/keyboard"}
-							/>
-							<GithubIcon url={"https://github.com/JobaF/dictionary-app"} />
-						</div>
-					</div>
-				</div>
-				<div className="relative mt-8">
-					<div className=" h-72 w-72 items-start justify-center overflow-hidden rounded-lg">
-						<div className="absolute inset-0  rounded-lg bg-gray-700 opacity-80 blur-md"></div>
-						<div className="relative inset-0 flex h-full w-full items-start justify-between rounded-md bg-clock bg-cover px-3 pt-2 text-left">
-							<span className="text-xl ">Age calculator</span>
-							<ArrowTopRightIcon
-								url={"https://datecalculator.jabofecht.com/"}
-							/>
-							<GithubIcon
-								url={"https://github.com/JobaF/frontendmentor-age-calculator"}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="relative mt-8">
-					<div className=" h-72 w-72 items-start justify-center overflow-hidden rounded-lg">
-						<div className="absolute inset-0  rounded-lg bg-yellow-600 opacity-70 blur-md"></div>
-						<div className="relative inset-0 flex h-full w-full items-start justify-between rounded-md bg-calculator bg-cover px-3 pt-2 text-left">
-							<span className="text-xl ">Apple-style calculator</span>
-							<ArrowTopRightIcon
-								url={"https://jobaf.github.io/odin-calculator/"}
-							/>
-							<GithubIcon url={"https://github.com/JobaF/odin-calculator"} />
-						</div>
-					</div>
-				</div>
+				))}
 			</section>
 		</div>
 	)
