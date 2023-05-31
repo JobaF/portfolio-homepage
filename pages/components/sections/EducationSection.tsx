43d68e37f6aece6c198cd2dece06219c77366c71
Jabo Fecht <jifecht@gmail.com>
2023-05-31 11:29:21 +0200

    Add better styling for project and work exp. section

--- pages/components/sections/EducationSection.tsx
+++ pages/components/sections/EducationSection.tsx
@@ -6,32 +6,38 @@ interface EducationSectionProps {}
 
 const EducationSection: FC<EducationSectionProps> = ({}) => {
 	return (
-		<section className="mb-10 mt-10 flex flex-col items-center" id="education">
-			<h1 className="mb-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-center text-4xl font-bold text-transparent">
-				Education
-			</h1>
-			<motion.div
-				variants={cardVariants}
-				initial="offscreen"
-				whileInView="onscreen"
-				viewport={{ once: true }}
-			>
-				{education.map((item, i) => (
-					<div
-						key={item.degree + i}
-						className="mt-4 flex w-80  flex-col rounded-lg bg-gray-900 p-6 shadow-md shadow-black"
-					>
-						<div className="text-lg">{item.degree}</div>
-						<div className="text-sm text-gray-400">{item.institution}</div>
-						<div className="text-sm text-gray-400">{item.period}</div>
-						<ul className="flex list-disc flex-col gap-1 pl-3 pt-2 text-sm">
-							{item.infos.map((info, i) => (
-								<li key={item.degree + i}>{info}</li>
-							))}
-						</ul>
-					</div>
-				))}
-			</motion.div>
+		<section className="mb-10 mt-10" id="education">
+			<div className="mx-auto flex max-w-4xl flex-col items-center px-3">
+				<h1 className="mb-4 w-fit bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-4xl font-bold text-transparent">
+					Education
+				</h1>
+				<motion.div
+					variants={cardVariants}
+					initial="offscreen"
+					whileInView="onscreen"
+					viewport={{ once: true }}
+				>
+					{education.map((item, i) => (
+						<div
+							key={item.degree + i}
+							className="mt-4 flex w-96 flex-col rounded-lg bg-gray-900 p-6 shadow-md shadow-black"
+						>
+							<div className="text-lg md:text-2xl">{item.degree}</div>
+							<div className="text-sm text-gray-400 md:text-lg">
+								{item.institution}
+							</div>
+							<div className="text-sm text-gray-400 md:text-lg">
+								{item.period}
+							</div>
+							<ul className="flex max-w-lg list-disc flex-col gap-1 pl-3 pt-2 text-sm md:text-lg">
+								{item.infos.map((info, i) => (
+									<li key={item.degree + i}>{info}</li>
+								))}
+							</ul>
+						</div>
+					))}
+				</motion.div>
+			</div>
 		</section>
 	)
 }
--- pages/components/sections/WorkExperienceSection.tsx
+++ pages/components/sections/WorkExperienceSection.tsx
@@ -6,30 +6,38 @@ interface WorkExperienceSectionProps {}
 
 const WorkExperienceSection: FC<WorkExperienceSectionProps> = ({}) => {
 	return (
-		<section className="mb-10 mt-10 flex flex-col items-center" id="experience">
-			<h1 className="mb-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-center text-4xl font-bold text-transparent">
-				Work Experience
-			</h1>
-			{workExperience.map((item, i) => (
+		<section className="mb-10 mt-10" id="experience">
+			<div className="mx-auto flex max-w-4xl flex-col items-center px-3">
+				<h1 className="mb-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-center text-4xl font-bold text-transparent">
+					Work Experience
+				</h1>
 				<motion.div
 					variants={cardVariants}
-					key={item.position + i}
 					initial="offscreen"
 					whileInView="onscreen"
 					viewport={{ once: true }}
 				>
-					<div className="mt-4 flex w-80 flex-col rounded-lg bg-gray-900 p-6 text-justify shadow-md shadow-black">
-						<div className="text-xl">{item.position}</div>
-						<div className="text-sm text-gray-400">{item.institution}</div>
-						<div className="text-sm text-gray-400">{item.period}</div>
-						<ul className="flex list-disc flex-col gap-1 pl-3 pt-2 text-sm">
-							{item.infos.map((info, i) => (
-								<li key={item.institution + i}>{info}</li>
-							))}
-						</ul>
-					</div>
+					{workExperience.map((item, i) => (
+						<div
+							key={item.position + i}
+							className="mt-4 flex w-96 flex-col rounded-lg bg-gray-900 p-6 text-justify shadow-md shadow-black"
+						>
+							<div className="text-lg md:text-2xl">{item.position}</div>
+							<div className="text-sm text-gray-400 md:text-lg">
+								{item.institution}
+							</div>
+							<div className="text-sm text-gray-400 md:text-lg">
+								{item.period}
+							</div>
+							<ul className="flex list-disc flex-col gap-1 pl-3 pt-2 text-sm md:text-lg">
+								{item.infos.map((info, i) => (
+									<li key={item.institution + i}>{info}</li>
+								))}
+							</ul>
+						</div>
+					))}
 				</motion.div>
-			))}
+			</div>
 		</section>
 	)
 }
